12af2a2924263d5b80a711a0407b8777fa970b6a
Rutva Argade <argade@sheridancollege.ca>
2022-04-14 18:44:17 -0400

    Made basic layout for joining a meeting page

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:videocalling_finalproject/utils/colors.dart';
 
 class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({Key? key}) : super(key: key);
@@ -8,8 +9,44 @@ class VideoCallScreen extends StatefulWidget {
 }
 
 class _VideoCallScreenState extends State<VideoCallScreen> {
+  late TextEditingController meetingIdController;
+
+  @override
+  void initState() {
+    meetingIdController = TextEditingController();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text("Join a Meeting", style:  TextStyle(
+          fontSize: 18,
+        ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+      child:TextField(
+            controller: meetingIdController,
+            maxLines: 1,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              fillColor: secondaryBackgroundColor,
+              filled: true,
+              border: InputBorder.none,
+              hintText: 'Room ID',
+              contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+            ),
+          ),
+          )
+        ]),
+    );
   }
 }
