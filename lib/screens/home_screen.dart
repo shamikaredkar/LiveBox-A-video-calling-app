ab887ea8d3d17067861ef989e48ffc28ad0d186b
pratham <prathamkoya@gmail.com>
2022-04-14 13:25:34 -0400

    HomeScreen bottomNavigation bar updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:videocalling_finalproject/utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,10 +9,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page){
+    setState(() {
+      _page = page;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Hello World"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        title: const Text("Meet & Chat"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        onTap: onPageChanged,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _page,
+        items:const [
+          BottomNavigationBarItem(icon: Icon(Icons.comment_bank),
+            label: "Meet & Chat"
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.lock_clock),
+              label: "Meetings"
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline),
+              label: "Contacts"
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings_outlined),
+              label: "Settings"
+          ),
+        ],
+      ),
     );
   }
 }
